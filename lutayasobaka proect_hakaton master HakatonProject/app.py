96380a9cc937289211bcff7a506d235f1b99cbff
lutayasobaka <167293947+lutayasobaka@users.noreply.github.com>
2024-12-11 15:57:35 +0300

    Add files via upload

---
+++ lutayasobaka proect_hakaton master HakatonProject/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, jsonify, render_template
+import requests
+from bs4 import BeautifulSoup
+
+app = Flask(__name__)
+
+def analyze_website(url, query):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    paragraphs = [p.get_text() for p in soup.find_all('p')]
+
+    results = [text for text in paragraphs if query.lower() in text.lower()]
+
+    return results
+
+@app.route('/')
+def index():
+    return render_template('analyze.html')
+
+@app.route('/analyze', methods=['POST'])
+def analyze():
+    data = request.json
+    url = data.get('url')
+    query = data.get('query')
+
+    found_items = analyze_website(url, query)
+
+    return jsonify(found_items)
+
+if __name__ == '__main__':
+    app.run(debug=True)
